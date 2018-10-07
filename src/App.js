02bca98fe3bb4fb43583517bc05527003763a9d6
bluerumex <bluerumex@naver.com>
2018-10-07 20:29:39 +0900

    AddColorForm property onNewColor 함수 수정

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import AddColorForm from './component/AddColorForm'
 
 const logColor = (title, color) =>
-    console.log(`새로운 색: ${title} | ${color}`);
+    console.log(`NEW COLOR: ${title} | ${color}`);
 class App extends Component {
     render() {
-        return <AddColorForm onNewColor={logColor}/> 
+        return <AddColorForm onNewColor={ (title, color) => {
+            console.log(`TODO: NEW COLOR Information ${title} | ${color} put on list...`);
+            console.log(`TODO: DISPLAY with NEW Color`);
+        } }/> 
     }
 }
 
