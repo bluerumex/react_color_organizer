2b2a45727613d378aae7c962372430262cbb8221
bluerumex <bluerumex@naver.com>
2018-10-08 16:47:08 +0900

    StarRating 표현 컴포넌트로 변경

--- src/component/StarRating.js
+++ src/component/StarRating.js
@@ -1,8 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import Star from './Star';
 import '../CSS/Rating.css'
 
+// 표현 컴포넌트로 변경
+const StarRating = ({ starsSelected=0, totalStars=5, onRate=f=>f}) =>
+    <div className='star-rating'>
+        {[...Array(totalStars)].map((n, i) => 
+            <Star key={i} selected={i<starsSelected} onClick={() => onRate(i+1)} />
+        )}
+        <p>Rating: {starsSelected} / {totalStars}</p>
+    </div> 
+
+/* 
 class StarRating extends Component {
 
     static propTypes = {
@@ -39,5 +48,6 @@ class StarRating extends Component {
         )
     };
 };
+ */
 
 export default StarRating;
